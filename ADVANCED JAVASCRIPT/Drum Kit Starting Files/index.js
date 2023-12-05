54e7e72bb77c37b5ec01764e38dceab589e0bf53
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2023-12-05 09:00:03 -0300

    Update index.js

--- ADVANCED JAVASCRIPT/Drum Kit Starting Files/index.js
+++ ADVANCED JAVASCRIPT/Drum Kit Starting Files/index.js
@@ -1,6 +1,6 @@
 let button = document.querySelectorAll(".drum")
 for (let pos in button){
-    button[pos].addEventListener('click', function(){
-        alert(pos)
+    button[pos].addEventListener('click',function(){
+
     })
 }
