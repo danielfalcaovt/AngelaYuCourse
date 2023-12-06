862271993a03009b3e66901eb60d93aa463fd47c
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2023-12-06 10:26:31 -0300

    update

--- ADVANCED JAVASCRIPT/Drum Kit Starting Files/index.js
+++ ADVANCED JAVASCRIPT/Drum Kit Starting Files/index.js
@@ -1,19 +1,11 @@
-let button = document.querySelectorAll(".drum")
-    var audios = ['crash.mp3','kick-bass.mp3','snare.mp3','tom-1.mp3','tom-2.mp3','tom-3.mp3','tom-4.mp3']
-    var sounds = []
-    for (let pos = 0; pos< 7; pos++){
-        sounds.push(new Audio(`./sounds/${audios[pos]}`))
-    }
-    async function playmusic(){
-        try {
-for (let pos in button){
-    button[pos].addEventListener('click',() => {
-
-                
-               await sounds[pos].play()
-               button.classList.add('playing')
-            }catch(err){
-                button.classList.remove('playing')
-            }
-        
+let audio = ['crash.mp3','kick-bass.mp3','snare.mp3','tom-1.mp3','tom-2.mp3','tom-3.mp3','tom-4.mp3']
+let sounds = []
+let numberButtons = document.querySelectorAll('.drum').length
+for (let ps in audio){
+    sounds.push(new Audio(`./sounds/${audio[ps]}`))
+}
+for (let pos = 0;pos<numberButtons;pos++){
+    document.querySelectorAll('.drum')[pos].addEventListener('click', function () {
+        this.style.color = 'white'
+    })
 }
