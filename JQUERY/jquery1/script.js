4e9492feadc457292fe3ae4ac83f8b36f281f617
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2023-12-06 15:10:14 -0300

    jQuery Module

---
+++ JQUERY/jquery1/script.js
@@ -0,0 +1,10 @@
+/* document.querySelector('h1') */
+
+$("h1").addClass("big-title margin-50")
+/* document.querySelectorAll("button") */
+
+$('h1').text('You\'ll')
+
+$('h1').html('<em>now</em>')
+
+$('a').attr("href","https://github.com/danielfalcaovt")
