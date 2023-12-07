a93fe3029a3c99fdf882db862a38ec1389803e73
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2023-12-07 17:59:53 -0300

    Update script.js

--- ADVANCED JAVASCRIPT/Project/Simon Game Challenge Starting Files/script.js
+++ ADVANCED JAVASCRIPT/Project/Simon Game Challenge Starting Files/script.js
@@ -1,52 +1,58 @@
-//3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-let userChosen = ""
-
-//5. At the top of the game.js file, create a new empty array called gamePattern.
 var gamePattern = [];
 
-//1. Inside game.js create a new function called nextSequence()
-function nextSequence() {
-    
-    //2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
-    var randomNumber = Math.floor(Math.random() * 4);
-    
-    //4. Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
-    var randomChosenColour = buttonColours[randomNumber];
-    
-    //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
-    gamePattern.push(randomChosenColour);
-    
-}
+//3. At the top of the game.js file, create a new empty array with the name userClickedPattern.
+var userClickedPattern = [];
 
-//ESCOLHA DO USUARIO
-//PADRAO DO JOGO
+//1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+$(".btn").click(function() {
 
-/* SE CADA ESCOLHA DO USUARIO FOR IGUAL
-    A CADA POSIÇAO DO PADRAO DO JOGO
-    ATE O FIM DO LAÇO ADICIONAR +1 COR
-    AO FINAL DO ARRAY
-*/
-
-nextSequence()
-
-function showPattern(){
-    for (let pos in gamePattern){
-    $("#"+gamePattern[pos]).
-    }
-}
-
-function game(){
-    for (let pos= 0; pos<gamePattern.length;){
-        $(".btn").click(function(){
-            userChosen = $(this).attr("id")    
-        })
-
-        if(userChosen == gamePattern[pos]){
-            pos++
-        }else{
-            break
+  //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+  var userChosenColour = $(this).attr("id");
+  //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+  userClickedPattern.push(userChosenColour);
+  console.log(gamePattern)
+    console.log(userClickedPattern)
+    let isTrue = ""
+    for (let pos in userClickedPattern){
+        for (let ps in gamePattern){
+            if (gamePattern[ps]!=userClickedPattern[pos]&& gamePattern.length == userClickedPattern.length){
+                
+            }else{
+                isTrue = true
+            }
         }
     }
+    if (isTrue == true){
+        userClickedPattern = []
+        nextSequence()
+    }else{
+        gamePattern = []
+    }
+
+  console.log(userClickedPattern);
+  });
+
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  
+  setTimeout(() => {
+      $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+      var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+      audio.play();
+    
+  }, 250);
 }
+
+$(document).keydown(function(event){
+    if (event.key == "A"){
+        nextSequence()
+        console.log(gamePattern)
+    }
+})
