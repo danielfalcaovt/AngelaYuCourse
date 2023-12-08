8d1d4896a05f33ee2f3970752c909af6ee2e8137
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2023-12-08 09:45:10 -0300

    Update script.js

--- ADVANCED JAVASCRIPT/Project/Simon Game Challenge Starting Files/script.js
+++ ADVANCED JAVASCRIPT/Project/Simon Game Challenge Starting Files/script.js
@@ -2,60 +2,65 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
-//3. At the top of the game.js file, create a new empty array with the name userClickedPattern.
 var userClickedPattern = [];
 
-//1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false;
+
+//2. Create a new variable called level and start at level 0.
+var level = 0;
+
+//1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
+$(document).keypress(function() {
+  if (!started) {
+
+    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 $(".btn").click(function() {
 
-    $(this).addClass("pressed")
-    setTimeout(() => {
-        $(this).removeClass("pressed")
-    }, 150);
-  //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
   var userChosenColour = $(this).attr("id");
-  //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
   userClickedPattern.push(userChosenColour);
- 
-  console.log(userClickedPattern)
-  if (gamePattern.length == userClickedPattern.length){
-      for (let pos in gamePattern){
-      for (let ps in userClickedPattern){
-          if (gamePattern[pos] == userClickedPattern[ps] && gamePattern == userClickedPattern){
-                alert("igual")
-                nextSequence()
-            }else{
-                alert("error")
-            }
-        }
-    }
+  checkAnswer()
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
 
-  }else{
-    
-  }
 
-  console.log(userClickedPattern);
-  });
 
 function nextSequence() {
+
+  //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+  level++;
+
+  //5. Inside nextSequence(), update the h1 with this change in the value of level.
+  $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  
-  setTimeout(() => {
-      $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-      var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-      audio.play();
-    
-  }, 250);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 }
 
-$(document).keydown(function(event){
-    if (event.key == "A"){
-        nextSequence()
-        console.log(gamePattern)
-    }
-})
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+
+
+}
