4ebc32c114517bc7d7845f855a073709a6f18819
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2023-12-16 13:24:07 -0300

    ending of backend module

--- BACKEND/API JSON/5.6 Secrets Project/index.js
+++ BACKEND/API JSON/5.6 Secrets Project/index.js
@@ -6,11 +6,17 @@ import axios from "axios";
 // 2. Create an express app and set the port number.
 const app = express()
 const port = 3000
+const API_URL = "https://secrets-api.appbrewery.com"
 // 3. Use the public folder for static files.
 app.use(express.static("public"))
 // 4. When the user goes to the home page it should render the index.ejs file.
-app.get("/", (req, res)=>{
-    res.render("index.ejs")
+app.get("/", async (req, res)=>{
+    const response = await axios.get(API_URL + "/random")
+    const result = response.data
+    res.render("index.ejs",{
+        secret:result.secret,
+        user:result.username
+    })
 })
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
@@ -19,3 +25,4 @@ app.get("/", (req, res)=>{
 app.listen(port, ()=>{
     console.log("Server running in port " + port);
 })
+
