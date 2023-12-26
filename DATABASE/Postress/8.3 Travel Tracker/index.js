34f5f601cf63a5d2853bc63dc3ad3126fe228033
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2023-12-26 14:51:25 -0300

    Database, Travel Tracker project

---
+++ DATABASE/Postress/8.3 Travel Tracker/index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const db = new pg.Client({
+  user: "postgres",
+  hostname: "localhost",
+  database: "worldwide",
+  password: "brbr109br",
+  port: "5432",
+});
+
+db.connect();
+
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+  });
+});
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+app.post("/add", async (req, res) => {
+  const request = req.body.country.trim();
+  const country = capitalizeFirstLetter(request)
+  console.log(country);
+  const c_code = await db.query(`SELECT country_code FROM countries WHERE country_name LIKE '${country}%'`);
+  const code = c_code.rows["0"].country_code
+  await db.query(`INSERT INTO visited_countries (country_code)
+                  VALUES ($1)`,[code]);
+
+  //OLD COUNTRIES
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
