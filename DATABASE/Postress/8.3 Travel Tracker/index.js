2fa362f887885d957a825f7eb5bc2f18281c0580
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2023-12-27 11:06:59 -0300

    Update index.js

--- DATABASE/Postress/8.3 Travel Tracker/index.js
+++ DATABASE/Postress/8.3 Travel Tracker/index.js
@@ -42,20 +42,37 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 app.post("/add", async (req, res) => {
+  // Get em todos os paises ja inseridos no banco de dados.
+  const countries = await checkVisited()
+  // Pegar o REQ do usuario, Capitalizar para procurar dentro do banco de dados de paises
   const request = req.body.country.trim();
   const country = capitalizeFirstLetter(request)
   console.log(country);
-  const c_code = await db.query(`SELECT country_code FROM countries WHERE country_name LIKE '${country}%'`);
-  if (c_code.rows.length !== 0){
+  try{
+  // Se o pais for encontrado dentro do banco de dados de paises, o codigo do pais sera retornado
+  const c_code = await db.query(`SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%$1%'`,[country.toLowerCase()]);
+  console.log(c_code.rows["0"]);
   const code = c_code.rows["0"].country_code
+  console.log(code);
+  try{
   await db.query(`INSERT INTO visited_countries (country_code)
                   VALUES ($1)`,[code]);
-             
-                  res.redirect("/");
-}else{
-  
-
-    res.redirect("/")
+                  res.redirect("/")
+     }catch(err){
+      console.log(err);
+       res.render("index.ejs",{
+        error:"Country has already been added.",
+        countries: countries,
+        total: countries.length
+      })
+     }   
+    }catch(err){
+      console.log(err);
+  res.render("index.ejs",{
+    error:"Country could not found.",
+    countries: countries,
+    total: countries.length
+  })
 }
 });
 
