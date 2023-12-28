a46949b27668ecd0c430b82bae4abe70d851dffd
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2023-12-28 15:07:23 -0300

    Family Tracker Project with PostgreSQL, Express etc.

--- ADVANCED JAVASCRIPT/Drum Kit Starting Files/index.js
+++ ADVANCED JAVASCRIPT/Drum Kit Starting Files/index.js
@@ -1,11 +1,53 @@
-let audio = ['crash.mp3','kick-bass.mp3','snare.mp3','tom-1.mp3','tom-2.mp3','tom-3.mp3','tom-4.mp3']
-let sounds = []
-let numberButtons = document.querySelectorAll('.drum').length
-for (let ps in audio){
-    sounds.push(new Audio(`./sounds/${audio[ps]}`))
+let audio = [
+  "crash.mp3",
+  "kick-bass.mp3",
+  "snare.mp3",
+  "tom-1.mp3",
+  "tom-2.mp3",
+  "tom-3.mp3",
+  "tom-4.mp3",
+];
+let sounds = [];
+let numberButtons = document.querySelectorAll(".drum").length;
+for (let ps in audio) {
+  sounds.push(new Audio(`./sounds/${audio[ps]}`));
 }
-for (let pos = 0;pos<numberButtons;pos++){
-    document.querySelectorAll('.drum')[pos].addEventListener('click', function () {
-        sounds[pos].play()
-    })
+for (let pos = 0; pos < numberButtons; pos++) {
+  document.querySelectorAll(".drum")
+    [pos].addEventListener("click", function () {
+      let buttonpressed = this.innerHTML;
+        sound(buttonpressed)
+    });
 }
+
+function sound(key) {
+  switch (key) {
+    case "w":
+      sounds[0].play();
+      break;
+    case "a":
+      sounds[1].play();
+      break;
+    case "s":
+      sounds[2].play();
+      break;
+    case "d":
+      sounds[3].play();
+      break;
+    case "j":
+      sounds[4].play();
+      break;
+    case "k":
+      sounds[5].play();
+      break;
+    case "l":
+      sounds[6].play();
+      break;
+  }
+}
+document.addEventListener("keypress", function (event) {
+    sound(event.key)
+});
+addEventListener("pageshow",function(event){
+    
+})
--- DATABASE/Postress/8.5 Family Travel Tracker/index.js
+++ DATABASE/Postress/8.5 Family Travel Tracker/index.js
@@ -8,8 +8,8 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "world",
-  password: "123456",
+  database: "worldwide",
+  password: "brbr109br",
   port: 5432,
 });
 db.connect();
@@ -20,12 +20,17 @@ app.use(express.static("public"));
 let currentUserId = 1;
 
 let users = [
-  { id: 1, name: "Angela", color: "teal" },
-  { id: 2, name: "Jack", color: "powderblue" },
+  { id: 1, name: "Daniel", color: "teal" },
+  { id: 2, name: "Isabella", color: "powderblue" },
 ];
 
-async function checkVisisted() {
-  const result = await db.query("SELECT country_code FROM visited_countries");
+async function checkVisisted(prop) {
+  const result = await db.query(`
+  SELECT *
+  FROM visited_countries 
+  JOIN whois 
+  ON whois.id = ${prop} and whois.id = visited_countries.person
+  `);
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
@@ -33,17 +38,26 @@ async function checkVisisted() {
   return countries;
 }
 app.get("/", async (req, res) => {
-  const countries = await checkVisisted();
+  const countries = await checkVisisted(currentUserId);
+  let usercolor = ""
+  for (let pos in users){
+    if (users[pos].id == currentUserId){
+      usercolor = users[pos].color;
+    };
+  };
+  console.log(users);
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
     users: users,
-    color: "teal",
+    color: usercolor,
   });
 });
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
-
+  const username = Number(currentUserId)
+  
+  console.log(username);
   try {
     const result = await db.query(
       "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
@@ -54,8 +68,8 @@ app.post("/add", async (req, res) => {
     const countryCode = data.country_code;
     try {
       await db.query(
-        "INSERT INTO visited_countries (country_code) VALUES ($1)",
-        [countryCode]
+        "INSERT INTO visited_countries (country_code,person) VALUES ($1,$2)",
+        [countryCode, username]
       );
       res.redirect("/");
     } catch (err) {
@@ -65,9 +79,37 @@ app.post("/add", async (req, res) => {
     console.log(err);
   }
 });
-app.post("/user", async (req, res) => {});
+app.post("/user", async (req, res) => {
+  currentUserId = Number(req.body.user);
+  console.log(currentUserId);
+  let usercolor = ""
+  for (let pos in users){
+    if (users[pos].id == currentUserId){
+      usercolor = users[pos].color;
+    };
+  };
+  const countries = await checkVisisted(currentUserId);
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    users: users,
+    color: usercolor,
+  });
+});
 
 app.post("/new", async (req, res) => {
+  try{
+    const color = req.body.color
+    const name = req.body.name
+    const newuser = await db.query(`INSERT INTO whois (first_name) VALUES ($1) RETURNING id`,[name])
+    users.push({ id: newuser.rows["0"].id, name: String(name), color: String(color) });
+    console.log(users);
+    res.render("new.ejs")
+  }catch(err){
+    console.log(err);
+  }
+  
+
   //Hint: The RETURNING keyword can return the data that was inserted.
   //https://www.postgresql.org/docs/current/dml-returning.html
 });
