2daf34b783e3d809532c102c77c4bdb9d68b6125
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2024-01-03 09:50:09 -0300

    Authentication

--- AUTHENTICATION & SECURITY/Secrets - Starting Code/app.js
+++ AUTHENTICATION & SECURITY/Secrets - Starting Code/app.js
@@ -4,7 +4,12 @@ import bodyParser from "body-parser";
 import pg from "pg";
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/userDB", {userNewUrlParser: true});
+async function main(){
+
+    await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+}
+
+main().catch(err => {console.log(err);})
 
 const app = express()
 const port = 3000
@@ -12,6 +17,14 @@ const port = 3000
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
 app.set('view engine','ejs')
+
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const User = new mongoose.model("User", userSchema)
 
 app.get("/",(req,res)=>{
     res.render("home.ejs")
@@ -26,7 +39,16 @@ app.get("/login",(req,res)=>{
 app.get("/register",(req,res)=>{
     res.render("register.ejs")
 })
+app.post("/register", async(req,res)=>{
+    
+    const newUser = new User({
+        email:req.body.username,
+        password:req.body.password
+    });
+    await newUser.save()
+    res.render("secrets.ejs")
 
+});
 
 app.listen(port,(req,res)=>{
     console.log("Server is running in " + port);
