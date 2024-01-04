6a87fcfbc6beedf1bde20eb6baedea532ef549c1
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2024-01-04 10:59:29 -0300

    NEW REACT MODULE.

---
+++ REACT/REACT-WEB/react-app/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <h1>Why am i still here ?</h1>
+  </React.StrictMode>
+);
