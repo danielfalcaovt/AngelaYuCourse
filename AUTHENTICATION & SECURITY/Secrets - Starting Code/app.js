6b19bc5004ab7822ffb37ffbf8e3d58d1250faef
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2024-01-25 11:43:57 -0300

    Authentication & Security Module

--- AUTHENTICATION & SECURITY/Secrets - Starting Code/app.js
+++ AUTHENTICATION & SECURITY/Secrets - Starting Code/app.js
@@ -2,54 +2,91 @@ import express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser";
 import pg from "pg";
-import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-async function main(){
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "users",
+  password: "brbr109br",
+  port: 5432,
+});
+db.connect();
 
-    await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
-}
+const app = express();
+const port = 3000;
+const saltRounds = 3;
 
-main().catch(err => {console.log(err);})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-const app = express()
-const port = 3000
-
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static("public"))
-app.set('view engine','ejs')
-
-
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
-})
-
-const User = new mongoose.model("User", userSchema)
-
-app.get("/",(req,res)=>{
-    res.render("home.ejs")
-})
-
-
-app.get("/login",(req,res)=>{
-    res.render("login.ejs")
-})
-
-
-app.get("/register",(req,res)=>{
-    res.render("register.ejs")
-})
-app.post("/register", async(req,res)=>{
-    
-    const newUser = new User({
-        email:req.body.username,
-        password:req.body.password
-    });
-    await newUser.save()
-    res.render("secrets.ejs")
-
+app.get("/", (req, res) => {
+  res.render("home.ejs");
 });
 
-app.listen(port,(req,res)=>{
-    console.log("Server is running in " + port);
-})
+app.get("/login", (req, res) => {
+  res.render("login.ejs", {});
+});
+let attempt = 0
+app.post("/login", async (req, res) => {
+    bcrypt.hash()
+    try {
+    const user = req.body.username;
+    const checkUser = await db.query("SELECT * FROM users WHERE email = $1", [
+      user,
+    ]);
+    if (checkUser.rows.length >= 1) {
+      const pass = req.body.password;
+      const userData = checkUser.rows[0];
+      if (userData.password === pass) {
+        res.render("secrets.ejs");
+        attempt = 0;
+      } else {
+        attempt++
+        console.log(attempt);
+        if (attempt === 5){
+            res.render("home.ejs");
+        }else{
+            res.render("login.ejs");
+        }
+    }
+    } else {
+      res.send("You might not registered.");
+    }
+  } catch (err) {
+    res.render("login.ejs");
+    console.log(err);
+  }
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const email = req.body.username;
+    const checkValue = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (checkValue.rows.length >= 1) {
+      res.send("You're already registered. Try logging in.");
+    } else {
+      const password = req.body.password;
+      const register = await db.query(
+        "INSERT INTO users(email,password) VALUES($1,$2)",
+        [email, password]
+      );
+      console.log(register);
+      res.render("secrets.ejs");
+    }
+  } catch (err) {
+    res.render("register.ejs");
+  }
+});
+
+app.listen(port, (req, res) => {
+  console.log("Server is running in " + port);
+});
