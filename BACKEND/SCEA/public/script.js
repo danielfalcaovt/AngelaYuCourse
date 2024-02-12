4c1010984b77a88a899e5c63a43699372f33415d
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2024-02-12 19:16:30 -0300

    SCEA Project 0.0.51

--- BACKEND/SCEA/public/script.js
+++ BACKEND/SCEA/public/script.js
@@ -1,2 +1,11 @@
 const copyright = new Date().getFullYear();
 document.querySelector(".copyright").innerHTML += `SCEA Copyright ${copyright} ©`;
+
+const menu = document.querySelector("header > nav");
+document.querySelector("#hamburguer").addEventListener("click",()=>{
+    menu.classList.toggle("a7k29loooppps");
+});
+
+function options() {
+    alert("Working.");
+};
